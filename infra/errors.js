52c6e632a5dddf9411ebb854cd5be95534538493
jpbrehula <jpbrehula@hotmail.com>
2026-04-14 13:24:31 -0400

    feat: padroniza endpoint /status e adiciona teste de método não permitido

--- infra/errors.js
+++ infra/errors.js
@@ -27,3 +27,32 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+// Classe de erro customizado para representar falhas internas do servidor (HTTP 500)
+// A ideia é padronizar erros inesperados sem expor detalhes sensíveis para o usuário
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    // Chama o construtor da classe nativa Error:
+    // - define uma mensagem genérica (segura para o usuário)
+    // - armazena o erro original em "cause" para debug interno
+    super("Método não permitido para este endpoint.");
+    //Subscreve a propiedade name
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP enviado é válido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  // Controla a serialização do erro na API:
+  // - Error padrão vira {} porque suas propriedades não são enumeráveis
+  // - toJSON define explicitamente o formato da resposta
+  // - retorno apenas a mensagem para não expor detalhes internos (stack, cause)
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,52 +1,66 @@
+//.   pega esse objeto dentro do next connect
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
 // Importo só o InternalServerError porque este arquivo só trata erro 500.
 // Evito importar "errors" inteiro para manter claro quais erros são usados aqui
 // e evitar dependências desnecessárias.
-import { InternalServerError } from "infra/errors";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
 
-async function status(request, response) {
-  try {
-    //  Data de atualização da API
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    // Pergunta ao Postgress QUAL É A VERSÃO
-    const databaseVersionResult = await database.query("SHOW server_version;");
-    const databaseVersion = databaseVersionResult.rows[0].server_version;
+router.get(getHandler);
 
-    // Pergunta ao Postgress QUAL É O MAXIMO DE CONEXÕES
-    const maxConnectionsResult = await database.query("SHOW max_connections;");
-    const maxConnections = Number(maxConnectionsResult.rows[0].max_connections);
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
 
-    //pergunta ao Postgress QUANTAS CONEXÕES ESTÃO SENDO USADAS AGORA
-    const usedConnectionResult = await database.query(`
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+
+  console.log("\n Erro dentro do catch do next-connect:");
+  console.error(publicErrorObject);
+
+  response.status(500).json(publicErrorObject);
+}
+
+async function getHandler(request, response) {
+  //  Data de atualização da API
+  const updatedAt = new Date().toISOString();
+
+  // Pergunta ao Postgress QUAL É A VERSÃO
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  // Pergunta ao Postgress QUAL É O MAXIMO DE CONEXÕES
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = Number(maxConnectionsResult.rows[0].max_connections);
+
+  //pergunta ao Postgress QUANTAS CONEXÕES ESTÃO SENDO USADAS AGORA
+  const usedConnectionResult = await database.query(`
     SELECT count (*)
     FROM pg_stat_activity
     WHERE datname = current_Database();
     `);
 
-    const usedConnections = Number(usedConnectionResult.rows[0].count);
+  const usedConnections = Number(usedConnectionResult.rows[0].count);
 
-    // Retorno do JSON
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: databaseVersion,
-          max_connections: maxConnections,
-          used_connections: usedConnections,
-        },
+  // Retorno do JSON
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: maxConnections,
+        used_connections: usedConnections,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controller:");
-    console.error(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
