7ae7062b6470680dfd522ee9357768ea8886d767
jpbrehula <jpbrehula@hotmail.com>
2026-04-16 17:38:39 -0400

    feat: add migration to create `users` table

---
+++ infra/migrations/1776369921732_create-users.js
@@ -0,0 +1,50 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    // For reference, GitHub limits usernames to 39 characters.
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+    },
+    // Why 254 in length? https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+    },
+
+    // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+
+    // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+  });
+
+  pgm.sql(`
+    CREATE UNIQUE INDEX users_username_lower_unique_idx
+      ON users (LOWER(username));
+  `);
+
+  pgm.sql(`
+    CREATE UNIQUE INDEX users_email_lower_unique_idx
+      ON users (LOWER(email));
+  `);
+};
+
+exports.down = false;
