74bf7a1379426280b36055c5094fb68633eaf507
jpbrehula <jpbrehula@hotmail.com>
2026-04-16 17:36:32 -0400

    feat: add `runPendingMigrations` method to `orchestrator`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -2,6 +2,7 @@ import retry from "async-retry";
 // Importa o módulo de banco (infra/database.js)
 // Ele tem a função database.query(...) que executa SQL no PostgreSQL
 import database from "infra/database.js";
+import migrator from "models/migrator.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -29,8 +30,13 @@ async function clearDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
+  runPendingMigrations,
 };
 export default orchestrator;
